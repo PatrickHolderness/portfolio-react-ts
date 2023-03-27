da99a0c2bb3c0e37e49724d0f832d5fb8087cab6
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2023-03-27 13:48:24 +0200

    added header component, ability to change between themes using  LocalStorage

---
+++ src/components/header/index.tsx
@@ -0,0 +1,58 @@
+import { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
+
+import Switch from 'react-switch';
+import { Spin as Hamburger } from 'hamburger-react';
+
+import Logo from '../../assets/logo.svg';
+import LINKS from '../../constants/LINKS';
+
+import {
+  Nav,
+  Navbar,
+  MobileIcon,
+  NavBtns,
+  NavItem,
+  NavLinks,
+  NavLogo,
+  NavMenu,
+  ToggleBtn,
+} from './styles';
+
+interface Props {
+  toggleTheme(): void;
+}
+
+const Header: React.FC<Props> = ({ toggleTheme }) => {
+  const { colors, title } = useContext(ThemeContext);
+
+  return (
+    <Navbar>
+      <Nav>
+        <NavLogo src={Logo} />
+        <MobileIcon>
+          <Hamburger />
+        </MobileIcon>
+        <NavMenu>
+          {LINKS.map(link => (
+            <NavItem key={link.id}>
+              <NavLinks to={link.name}>{(link.name)}</NavLinks>
+            </NavItem>
+          ))}
+        </NavMenu>
+        <NavBtns>
+          <Switch
+            onChange={toggleTheme}
+            checked={title === 'dark'}
+            checkedIcon={false}
+            uncheckedIcon={false}
+            offColor={colors.accent}
+            onColor={colors.textSecondary}
+          />
+          <ToggleBtn>d/l</ToggleBtn>
+        </NavBtns>
+      </Nav>
+    </Navbar>
+  );
+};
+export default Header;
